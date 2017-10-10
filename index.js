b0b5ce30d3b3902f3d605dcf7f72dbee4466a70b
Serhii Kuzmychov <ku3mich@gmail.com>
2017-10-10 17:28:59 +0300

    added NotImplementedError

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ Object.assign(
     Object : require('./lib/object'),
     prototypeChain : require('./lib/prototypeChain'),
     RethrownError : require('./lib/rethrownError'),
+    NotImplementedError : require('./lib/notImplementedError'),
     EmitterLike : require('./lib/emitterLike')
   },
   require('./lib/pipe')
