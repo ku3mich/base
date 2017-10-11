94c2a454522dff24bbf47bf48735cf7e1932c95c
Serhii Kuzmychov <ku3mich@gmail.com>
2017-10-11 07:38:23 +0300

    prototypeChain: returns prototypes(not ctors) but can accept instance/ctor

--- test/prototypeChain.js
+++ test/prototypeChain.js
@@ -1,17 +1,26 @@
 const prototypeChain = require('../lib/prototypeChain');
 
+class B {
+}
+
+class C extends B {
+}
+
 describe('prototypeChain', function () {
-  it('correct', () =>{
-    class B {
-    }
-
-    class C extends B {
-    }
-
+  it('type', () =>{
     const chain = Array.from(prototypeChain(C));
     chain
       .should
-      .containEql(B)
+      .containEql(B.prototype)
+      .have
+      .length(2);
+  });
+
+  it('instace', () =>{
+    const chain = Array.from(prototypeChain(new C()));
+    chain
+      .should
+      .containEql(B.prototype)
       .have
       .length(2);
   });
